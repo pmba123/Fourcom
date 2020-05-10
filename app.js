92bb6046418193a349bab6e1cf966530ee8736e4
Phillip Almind <eaapmba@students.eaaa.dk>
2020-05-10 21:33:15 +0200

    Initial Upload

---
+++ app.js
@@ -0,0 +1,63 @@
+
+// Tutorial fulgt: https://www.youtube.com/watch?v=wlVmmsMD28w&t=778s
+
+// Vis side funktion
+$(document).ready(function(){
+    $('.checkBtn').click(function(){
+        $('.wrapmobil').toggleClass('active')
+    })
+
+    function showPage(pageId){
+        console.log('show'+ data-target);
+        $('.page').hide();
+        $(pageId).show();
+    }
+});
+
+
+
+
+
+
+/*const app = {
+    pages:[],
+    show: new Event('show'),
+    init: function(){
+        app.pages = document.querySelectorAll('.page');
+        app.pages.forEach((pg)=>{
+            pg.addEventListener('show', app.pageShown);
+        })
+
+        document.querySelectorAll('.nav-link').forEach((link)=>{
+            link.addEventListener('click', app.nav);
+        })
+        history.replaceState({},'hjem', '#home');
+        window.addEventListener('popstate', app.poppin);
+    },
+    nav: function(ev){
+        ev.preventDefault();
+        let currentPage = ev.target.getAttribute('data-target');
+        document.querySelector('active').classList.remove('active');
+        document.getElementById(currentPage).classList.add('active');
+        console.log(currentPage)
+        history.pushState({}, currentPage, `#${currentPage}`);
+        document.getElementById(currentPage).dispatchEvent(app.show);
+    },
+    pageShown: function(ev){
+        console.log('Page', ev.target.id, 'just shown')
+        let h1 = ev.target.querySelector('h1');
+
+    },
+    poppin: function(ev){
+        console.log(location.hash, 'popstate event')
+        let hash = location.replace('#','');
+        document.querySelector('active').classList.remove('active');
+        document.getElementById(hash).classList.add('active');
+        console.log(hash)
+        //history.pushState({}, currentPage, `#${currentPage}`);
+        document.getElementById(hash).dispatchEvent(app.show);
+    },
+
+}
+
+// document.addEventListener('DOMContentLoaded', app.init); */
---
+++ scroll.js
@@ -0,0 +1,21 @@
+ //Paralax scroll fundet på https://webmadewell.com/parallax-div-elements-scrolling-effect-with-jquery-using-css-transform/ d. 10/05/2020. 
+$(document).ready(function() {
+  //parallax scroll
+  $(window).on("load scroll", function() {
+    var parallaxElement = $(".parallax_scroll"),
+      parallaxQuantity = parallaxElement.length;
+    window.requestAnimationFrame(function() {
+      for (var i = 0; i < parallaxQuantity; i++) {
+        var currentElement = parallaxElement.eq(i),
+          windowTop = $(window).scrollTop(),
+          elementTop = currentElement.offset().top,
+          elementHeight = currentElement.height(),
+          viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
+          scrolled = windowTop - elementTop + viewPortHeight;
+        currentElement.css({
+          transform: "translate3d(0," + scrolled * -0.15 + "px, 0)"
+        });
+      }
+    });
+  });
+});
